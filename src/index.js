21e8f250c890c921d4b6f6572e10a2f0f783943c
Amar Nath Yogi <immortal2907@gmail.com>
2020-04-10 23:57:35 +0530

    Building project skeleton

--- src/index.js
+++ src/index.js
@@ -1,15 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// React imports
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+// redux Imports
+import { createStore, applyMiddleware, combineReducers } from "redux";
+import thunk from "redux-thunk";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+
+// CSS Imports
+import "semantic-ui-css/semantic.min.css";
+import "./index.css";
+
+// General Imports
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const composeEnhancers = composeWithDevTools({
+  // Specify name here, actionsBlacklist, actionsCreators and other options if needed
+});
+
+let middleware = composeEnhancers(applyMiddleware(thunk));
+if (process.env.NODE_ENV === "production") {
+  middleware = applyMiddleware(thunk);
+}
+
+const rootReducer = combineReducers({});
+
+const store = createStore(rootReducer, middleware);
+
+const app = (
+  <Provider store={store}>
+    <BrowserRouter basename="/BurgerApp">
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
+ReactDOM.render(app, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
