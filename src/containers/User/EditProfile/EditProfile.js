6121fd8d8467f6392ba2e769526879c61e749423
Amar Nath Yogi <immortal2907@gmail.com>
2020-05-15 00:47:11 +0530

    Added ChangePassword form to EditProfile Component

--- src/containers/User/EditProfile/EditProfile.js
+++ src/containers/User/EditProfile/EditProfile.js
@@ -1,8 +1,22 @@
 import React, { Component } from "react";
-import { Modal, Form, Message } from "semantic-ui-react";
+import {
+  Modal,
+  Form,
+  Message,
+  List,
+  Button,
+  Segment,
+  Menu,
+} from "semantic-ui-react";
 
 import ControlledInput from "../../../components/UI/ControlledInput/ControlledInput";
-import { updateObject, isEmail } from "../../../shared/utilities";
+import {
+  updateObject,
+  isEmail,
+  hasSmall,
+  hasCapital,
+  hasNumber,
+} from "../../../shared/utilities";
 import axios from "../../../axios";
 import {
   editProfile,
@@ -18,6 +32,7 @@ class EditProfile extends Component {
     super(props);
     const { currentUser } = props;
     this.state = {
+      activeItem: "Details",
       controls: {
         username: {
           valid: true,
@@ -37,8 +52,27 @@ class EditProfile extends Component {
         },
       },
       valid: false,
+      password: {
+        value: "",
+        valid: false,
+        capital: false,
+        small: false,
+        number: false,
+        long: false,
+      },
+      repeatPassword: {
+        value: "",
+      },
+      passwordValid: false,
+      showPassword: false,
+      passwordLoading: false,
+      passwordSuccess: false,
+      passwordError: "",
     };
   }
+  handleItemClick = (e, { name }) => {
+    this.setState({ activeItem: name });
+  };
   componentDidUpdate(nextProps) {
     let { currentUser } = this.props;
     if (nextProps.currentUser !== currentUser) {
@@ -65,6 +99,46 @@ class EditProfile extends Component {
       });
     }
   }
+
+  onPasswordChange = (e) => {
+    let value = e.target.value.trim(),
+      small = false,
+      capital = false,
+      number = false,
+      long = false;
+    small = hasSmall(value);
+    capital = hasCapital(value);
+    number = hasNumber(value);
+    long = value.length >= 8;
+    let valid = small && capital && number && long;
+    let password = {
+      value,
+      small,
+      capital,
+      number,
+      long,
+      valid,
+    };
+    let passwordValid = false;
+    passwordValid =
+      password.valid && password.value === this.state.repeatPassword.value;
+    this.setState({
+      password,
+      passwordValid,
+    });
+  };
+
+  onRepeatPasswordChange = (e) => {
+    let value = e.target.value.trim();
+    let { password } = this.state;
+    let repeatPassword = { value };
+    let passwordValid =
+      password.valid && password.value === repeatPassword.value;
+    this.setState({
+      repeatPassword,
+      passwordValid,
+    });
+  };
 
   checkValidity = (username, about_me) => {
     let valid = false;
@@ -209,9 +283,73 @@ class EditProfile extends Component {
     this.props.onEmailUpdate(email.value, this.props.token);
   };
 
+  onPasswordSave = () => {
+    let passwordLoading = true,
+      passwordError = "",
+      passwordSuccess = false,
+      config = {
+        headers: {
+          Authorization: "Bearer " + this.props.token,
+        },
+      },
+      data = {
+        password: this.state.password.value,
+      };
+    axios
+      .post("/users/change_password", data, config)
+      .then((res) => {
+        if (res.data.success) {
+          passwordLoading = false;
+          passwordSuccess = true;
+        }
+        this.setState({
+          password: {
+            value: "",
+            valid: false,
+            capital: false,
+            small: false,
+            number: false,
+            long: false,
+          },
+          repeatPassword: {
+            value: "",
+          },
+          passwordLoading,
+          passwordSuccess,
+        });
+      })
+      .catch((err) => {
+        let error = "Unexpected Error";
+        if (err.response) {
+          error = err.response.message;
+        }
+        passwordLoading = false;
+        passwordError = error;
+        this.setState({
+          passwordError,
+          passwordLoading,
+        });
+      });
+    this.setState({
+      passwordLoading,
+      passwordError,
+      passwordSuccess,
+    });
+  };
+
   render() {
     const { username, email, about_me } = this.state.controls;
     const { emailError, emailLoading, emailData } = this.props;
+    const {
+      password,
+      showPassword,
+      repeatPassword,
+      passwordValid,
+      passwordLoading,
+      passwordError,
+      passwordSuccess,
+      activeItem,
+    } = this.state;
     return (
       <Modal
         defaultOpen
@@ -221,76 +359,179 @@ class EditProfile extends Component {
         closeIcon
       >
         <Modal.Header>Edit Profile</Modal.Header>
-        <Modal.Content>
+        <Modal.Content scrolling>
           {!this.props.loading && this.props.error ? (
             <Message error>{this.props.error}</Message>
           ) : null}
-          <Form error={username.error ? true : false}>
-            <Form.Input
-              fluid
-              icon="user"
-              placeholder="Username"
-              label="Username"
-              type="text"
-              value={username.value}
-              loading={username.loading}
-              error={!username.loading && !username.valid}
-              onChange={this.onUsernameChange}
+          <Menu attached="top" tabular>
+            <Menu.Item
+              name="Details"
+              active={activeItem === "Details"}
+              onClick={this.handleItemClick}
             />
-            {username.error ? <Message error>{username.error}</Message> : null}
-            <Form.Field>
-              <label>About Me</label>
-              <ControlledInput
-                placeHolder="Tell us about yourself"
-                minLength={0}
-                maxLength={140}
-                value={about_me.value}
-                onInput={this.onAboutChange}
+            <Menu.Item
+              name="Email"
+              active={activeItem === "Email"}
+              onClick={this.handleItemClick}
+            />
+            <Menu.Item
+              name="Password"
+              active={activeItem === "Password"}
+              onClick={this.handleItemClick}
+            />
+          </Menu>
+          {activeItem === "Details" ? (
+            <Form
+              as={Segment}
+              attached="bottom"
+              error={username.error ? true : false}
+            >
+              <Form.Input
+                fluid
+                icon="user"
+                placeholder="Username"
+                label="Username"
+                type="text"
+                value={username.value}
+                loading={username.loading}
+                error={!username.loading && !username.valid}
+                onChange={this.onUsernameChange}
               />
-            </Form.Field>
-            <Form.Button
-              fluid
-              positive
-              loading={this.props.loading}
-              disabled={!this.state.valid}
-              onClick={this.onSave}
+              {username.error ? (
+                <Message error>{username.error}</Message>
+              ) : null}
+              <Form.Field>
+                <label>About Me</label>
+                <ControlledInput
+                  placeHolder="Tell us about yourself"
+                  minLength={0}
+                  maxLength={140}
+                  value={about_me.value}
+                  onInput={this.onAboutChange}
+                />
+              </Form.Field>
+              <Form.Button
+                fluid
+                positive
+                loading={this.props.loading}
+                disabled={!this.state.valid}
+                onClick={this.onSave}
+              >
+                Save
+              </Form.Button>
+            </Form>
+          ) : null}
+          {activeItem === "Email" ? (
+            <Form
+              as={Segment}
+              attached="bottom"
+              error={email.error || emailError ? true : false}
             >
-              Save
-            </Form.Button>
-          </Form>
-          <hr />
-          <Form error={email.error || emailError ? true : false}>
-            <Message color="teal">
-              Changing Email requires OTP verification.
-            </Message>
-            <Form.Input
-              fluid
-              icon="mail"
-              placeholder="E-mail Address"
-              label="E-mail"
-              type="text"
-              value={email.value}
-              loading={email.loading}
-              error={
-                !email.loading &&
-                !email.valid &&
-                email.value !== this.props.currentUser.email
-              }
-              onChange={this.onEmailChange}
-            />
-            {email.error ? <Message error>{email.error}</Message> : null}
-            <Form.Button
-              fluid
-              positive
-              disabled={!email.valid}
-              onClick={this.onEmailSave}
-              loading={emailLoading}
-            >
-              Change Email
-            </Form.Button>
-            {emailError ? <Message error>{emailError}</Message> : null}
-            {emailData ? <VerifyUpdate data={emailData} /> : null}
-          </Form>
+              <Message color="teal">
+                Changing Email requires OTP verification.
+              </Message>
+              <Form.Input
+                fluid
+                icon="mail"
+                placeholder="E-mail Address"
+                label="E-mail"
+                type="text"
+                value={email.value}
+                loading={email.loading}
+                error={
+                  !email.loading &&
+                  !email.valid &&
+                  email.value !== this.props.currentUser.email
+                }
+                onChange={this.onEmailChange}
+              />
+              {email.error ? <Message error>{email.error}</Message> : null}
+              <Form.Button
+                fluid
+                positive
+                disabled={!email.valid}
+                onClick={this.onEmailSave}
+                loading={emailLoading}
+              >
+                Change Email
+              </Form.Button>
+              {emailError ? <Message error>{emailError}</Message> : null}
+              {emailData ? <VerifyUpdate data={emailData} /> : null}
+            </Form>
+          ) : null}
+          {activeItem === "Password" ? (
+            <Form error as={Segment} attached="bottom">
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Password"
+                type="password"
+                onChange={this.onPasswordChange}
+                value={password.value}
+                action={{
+                  icon: showPassword ? "eye slash" : "eye",
+                  onClick: () => this.setState({ showPassword: !showPassword }),
+                }}
+              />
+              {showPassword ? (
+                <Form.Input value={password.value} disabled />
+              ) : null}
+              <List style={{ textAlign: "left" }}>
+                <List.Item
+                  icon={
+                    password.small ? "check circle" : "check circle outline"
+                  }
+                  content="contains small letter"
+                />
+                <List.Item
+                  icon={
+                    password.capital ? "check circle" : "check circle outline"
+                  }
+                  content="contains capital letter"
+                />
+                <List.Item
+                  icon={
+                    password.number ? "check circle" : "check circle outline"
+                  }
+                  content="contains a digit"
+                />
+                <List.Item
+                  icon={password.long ? "check circle" : "check circle outline"}
+                  content="contains atleast 8 characters"
+                />
+              </List>
+              <Form.Input
+                fluid
+                icon="lock"
+                iconPosition="left"
+                placeholder="Repeat Password"
+                type="password"
+                onChange={this.onRepeatPasswordChange}
+                value={repeatPassword.value}
+                error={
+                  password.valid && !(repeatPassword.value === password.value)
+                }
+              />
+              {showPassword ? (
+                <Form.Input value={repeatPassword.value} disabled />
+              ) : null}
+              <Button
+                positive
+                fluid
+                content="Change Password"
+                loading={passwordLoading}
+                disabled={passwordLoading || !passwordValid}
+                onClick={this.onPasswordSave}
+              />
+              {!passwordLoading && passwordError ? (
+                <Message error>{passwordError}</Message>
+              ) : null}
+              {passwordSuccess ? (
+                <Message positive>Password saved!</Message>
+              ) : null}
+            </Form>
+          ) : null}
         </Modal.Content>
       </Modal>
     );
