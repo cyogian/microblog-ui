029aa561f53288138c995c2f7495b59d2154ceb1
Amar Nath Yogi <contact@cyogian.dev>
2021-03-16 23:48:08 +0530

    Added important Note on Landing Page

--- src/containers/Landing/Landing.js
+++ src/containers/Landing/Landing.js
@@ -17,6 +17,11 @@ const WelcomeContent = (
   <Container className={classes.Welcome} textAlign="center">
     <Header as="h1" content="Welcome to Microblog!" />
     <Message
+      color="white"
+    >
+      <h3 style={{ color: "red" }}>NOTE</h3>
+    The "Backend Server" for this WebApp is deployed on a "FreeTier Heroku Dyno", which turns off if there is no activity for a certain time-period. So while sending the first request the site will wait for about a minute for server to start & respond. After that, the site will start responding quickly. Also, this site requires you to register an account with valid email to use it. Inserting a false email won't help because, registration requires an OTP which will be sent to your unique email.</Message>
+    <Message
       as="h3"
       color="teal"
       content="This is a microblogging website where you can post text messages which can
